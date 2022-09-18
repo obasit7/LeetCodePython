fffb26c36a273bb5bdf1f4702d1a24922fae2c00
obasit7 <basitosama@gmail.com>
2022-09-18 01:34:20 -0400

    converted to function

--- max_subarray.py
+++ max_subarray.py
@@ -1,10 +1,14 @@
 arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
-max_sum = arr[0]
-curr_sum = 0
 
-for n in arr:
-    if curr_sum < 0:
-        curr_sum = 0
-    curr_sum += n
-    max_sum = max(max_sum, curr_sum)
+def max_sum_subarray(arr):
+    max_sum = arr[0]
+    curr_sum = 0
+
+    for n in arr:
+        if curr_sum < 0:
+            curr_sum = 0
+        curr_sum += n
+        max_sum = max(max_sum, curr_sum)
+
+    return max_sum
